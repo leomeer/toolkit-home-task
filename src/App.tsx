882bce4c9ec95f9e93b0286dfaba21f48fee09b1
leomeer <lmeerovich@gmail.com>
2024-02-16 10:14:50 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,23 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Test for <a href="http://www.tool-kit.tech" target="new">tool-kit.tech</a></h1>
+      <h1>Hometask for <a href="http://www.tool-kit.tech" target="new">tool-kit.tech</a></h1>
+
+    <div>
+      <table border={1} width={300}>
+      <tr><th  align='center'>Repo name</th><th>url</th><th>Owner</th></tr>
+      <tr><td>Repo 1</td><td><a href='#'>link</a></td><td>user</td></tr>
+      <tr><td>Repo 2</td><td><a href='#'>link</a></td><td>user</td></tr>
+      <tr><td>Repo 3</td><td><a href='#'>link</a></td><td>user</td></tr>
+      <tr><td>Repo 4</td><td><a href='#'>link</a></td><td>user</td></tr>
+      <tr><td>Repo 5</td><td><a href='#'>link</a></td><td>user</td></tr>
+      <tr><td>Repo 6</td><td><a href='#'>link</a></td><td>user</td></tr>
+      <tr><td>Repo 7</td><td><a href='#'>link</a></td><td>user</td></tr>
+      </table>
+
+    </div>
+
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
